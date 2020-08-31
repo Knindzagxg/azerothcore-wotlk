d2ceab4bd153afdff5dab54db081a7540e827888
Knindza <knindzagxg@gmail.com>
2020-08-31 14:58:13 +0200

    [Build] Emblems Update

---
+++ data/sql/updates/pending_db_world/rev_1598878663459173800.sql
@@ -0,0 +1,14 @@
+INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1598878663459173800');
+/*
+ * General: Build Update
+ * Update by Knindza | <www.azerothcore.org>
+ * Copyright (C) <www.shadowburn.net> & <www.lichbane.com>
+*/
+
+/* EMBLEMS */ 
+
+UPDATE `item_template` SET `VerifiedBuild` = 9183 WHERE `entry` = 40752;
+UPDATE `item_template` SET `VerifiedBuild` = 9183 WHERE `entry` = 40753;
+UPDATE `item_template` SET `VerifiedBuild` = 9767 WHERE `entry` = 45624;
+UPDATE `item_template` SET `VerifiedBuild` = 10314 WHERE `entry` = 47241;
+UPDATE `item_template` SET `VerifiedBuild` = 11159 WHERE `entry` = 49426;
